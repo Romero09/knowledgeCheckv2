8e211084b8c28e349a37d8abb8c70943a7ed3bc3
Pavels Vetlugins <pavels.vetlugins@example.com>
2018-07-17 10:26:27 +0300

    v2.1 With working server

---
+++ src/main/java/bootcamp/KCV2/Server/ServerApplication.java
@@ -0,0 +1,20 @@
+package bootcamp.KCV2.Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class ServerApplication {
+
+	public static void main(String[] args) throws Exception {
+		//  uncomment next line to start JettyApplication web app:
+		SpringApplication.run(ServerApplication.class, args);
+		// Look at
+		// http://docs.spring.io/spring-boot/docs/current/api/org/springframework/boot/SpringApplication.html
+		// for more information.
+	}
+}
