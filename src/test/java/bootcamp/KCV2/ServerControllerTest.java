486859e62ef9a8152c72f471912263ded43667f6
Pavels Vetlugins <pavels.vetlugins@example.com>
2018-07-24 23:06:25 +0300

    Try to JUnit serverController

--- src/test/java/bootcamp/KCV2/ServerControllerTest.java
+++ src/test/java/bootcamp/KCV2/ServerControllerTest.java
@@ -1,46 +1,43 @@
 package bootcamp.KCV2;
 
-import static org.junit.Assert.assertFalse;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.log4j.Logger;
+import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import bootcamp.kcv2.QuestionManager;
 import bootcamp.kcv2.server.ServerController;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringRunner.class)
 public class ServerControllerTest extends Mockito{
 
-	final Logger log = Logger.getLogger(ServerControllerTest.class);
-	public static ServerController controller ;
+	private MockMvc mockMvc;
 	
-	@Test
-	public final void test01homePage() {
-		 HttpServletRequest request = mock(HttpServletRequest.class);       
-	        HttpServletResponse response = mock(HttpServletResponse.class);
-		
-		
+	@InjectMocks
+	private ServerController controller;
+	
+	@Before
+	public void setup() throws Exception{
+		mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
 	}
 	
 	@Test
-	public final void test01isExamStarted() {
-		 HttpServletRequest request = mock(HttpServletRequest.class);       
-	        HttpServletResponse response = mock(HttpServletResponse.class);
-	        
-	        when(request.getParameter("userCode")).thenReturn("pxzX");
-	        
-		try {
-			controller = new ServerController();
-			controller.questionsPage("userCode", request, response);
-		} catch (Exception e) {
-		}
+	public final void test01isExamStarted() throws Exception {
+		System.err.println("pizda");
+		 mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
 
 }
