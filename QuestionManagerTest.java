09272c38579a7d21309a4dd6128038d8d5668399
Deniss <30836373+TiPSYDiPSY@users.noreply.github.com>
2018-07-25 16:26:57 +0300

    Add files via upload

---
+++ QuestionManagerTest.java
@@ -0,0 +1,219 @@
+package bootcamp.KCV2;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.apache.log4j.Logger;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import bootcamp.kcv2.Question;
+import bootcamp.kcv2.QuestionManager;
+import bootcamp.kcv2.QuestionTypes;
+import bootcamp.kcv2.StudentAnswerSheet;
+
+/**
+ *This class provides Testing for class {@link bootcamp.kcv2.QuestionManager  QuestionManager}.
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class QuestionManagerTest {
+	final Logger log = Logger.getLogger(QuestionManagerTest.class);
+	public static QuestionManager manager;
+	
+	
+	/**
+	 * This method tests if exam is started.
+	 */
+	@Test
+	public final void test01isExamStarted() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.setExamStarted(false);
+			assertFalse(manager.isExamStarted());
+		} catch (Exception e) {
+		}
+	}
+    /**
+     * This method try to start exam.
+     */
+	@Test
+	public final void test02setExamStarted() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.setExamStarted(true);
+			Assert.assertEquals(manager.isExamStarted(), true);
+			manager.setExamStarted(false);
+			Assert.assertEquals(manager.isExamStarted(), false);
+		} catch (Exception e) {
+		}
+	}
+	
+	  /**
+	   * This method tries to get current question bundle.
+	   */
+	@Test
+	public final void test03getCurrentQuestionBundle() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.getCurrentQuestionBundle();
+			Assert.assertNotNull(manager.getCurrentQuestionBundle());
+		} catch (Exception e) {
+		}
+	}
+
+	/**
+	 * This method tries to set current question bundle.
+	 */
+	@Test
+	public final void test04setCurrentQuestionBundle() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.setCurrentQuestionBundle("test4");
+			Assert.assertEquals(manager.getCurrentQuestionBundle(), "test4");
+		} catch (Exception e) {
+		}
+	}
+    /**
+     * This method tests submitting.
+     */
+	@Test
+	public final void test05submitResults() {
+		try {
+			ArrayList<String> answers = new ArrayList<String>();
+			answers.add("A");
+			ArrayList<Question> alQuestions = manager.pullQuestionBundle(manager.getCurrentQuestionBundle());
+
+			String userCode = "AE58";
+			manager = QuestionManager.getInstance();
+			manager.submitResults(userCode, answers, alQuestions);
+			log.info(userCode);
+			log.info(answers);
+			log.info(alQuestions);
+		} catch (Exception e) {
+		}
+	}
+    /**
+     *  This method tries to get bundle names.
+     */
+	@Test
+	public final void test06pullBundleNames() {
+		try {
+			manager = QuestionManager.getInstance();
+			ArrayList<String> bundleNames = manager.pullBundleNames();
+			log.info(bundleNames);
+		} catch (Exception e) {
+		}
+	}
+    /**
+     * This method tries to insert question.
+     */
+	@Test
+	public final void test07insertQuestion() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.setCurrentQuestionBundle("test4");
+			ArrayList<Question> questions = manager.pullQuestionBundle(manager.getCurrentQuestionBundle());
+
+		} catch (Exception e) {
+		}
+	}
+    /**
+     * Same as {@link #test07insertQuestion() testinsertQuestion} 
+     */
+	@Test
+	public final void test071insertQuestion() {
+		try {
+			manager = QuestionManager.getInstance();
+
+			ArrayList<Question> questions = manager.pullQuestionBundle(manager.getCurrentQuestionBundle());
+
+		} catch (Exception e) {
+		}
+	}
+    /**
+     *  This method checks results.
+     */
+	@Test
+	public final void test08resultsCheck() {
+		try {
+			ArrayList<String> answers = new ArrayList<String>();
+			ArrayList<Question> alQuestions = manager.pullQuestionBundle(manager.pullBundleNames().get(0));
+			String userCode = "AE58";
+			manager.resultsCheck(userCode, answers, alQuestions);
+			for (int i = 0; i < alQuestions.size(); i++) {
+				if(i == 0){
+					answers.add(Question.answersGrouping(alQuestions.get(i).getCorrectAnswers())+"-WRONG");
+					continue;
+				}
+				if(i == alQuestions.size()-1){
+					answers.add(Question.answersGrouping(alQuestions.get(i).getCorrectAnswers())+"===");
+					continue;
+				}
+				answers.add(Question.answersGrouping(alQuestions.get(i).getCorrectAnswers()));
+			}
+			Assert.assertEquals(manager.resultsCheck(userCode, answers, alQuestions), "2/3 66%, Wrong answers on questions: [1].");
+		} catch (Exception e) {
+		}
+	}
+    /**
+     *  This method sets exam duration.
+     */
+	@Test
+	public final void test09setExamDuration() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.setExamDuration(20);
+			Assert.assertEquals(manager.getExamDuration(), 20);
+		} catch (Exception e) {
+		}
+	}
+     /**
+      * This method tries to get question bundle.
+      */
+	@Test
+	public final void test10getQuestionBundle() {
+		try {
+			manager = QuestionManager.getInstance();
+			manager.getQuestionBundle("AE58");
+			manager.getQuestionBundle("AE58");
+		} catch (Exception e) {
+		}
+	}
+        /**
+         * This method test questions types.
+         */
+	@Test
+	public final void test11QuestionTypes() {
+
+		QuestionTypes[] a = QuestionTypes.values();
+		assertTrue(a != null);
+	}
+	  /**
+	   *  Same as {@link #test11QuestionTypes() testQuestionTypes} 
+	   */
+	@Test
+	public final void test12QuestionTypes() {
+
+		Question test = new Question(0, null, 0, null,"MULTI_CHOICE", null, null);
+		
+		ArrayList<String> answersVar = new ArrayList<String>();
+		ArrayList<String> correctAnswers = new ArrayList<String>();
+		answersVar.add("A");
+		correctAnswers.add("A");
+		test.setId(1);
+		test.setSet("test 1");
+		test.setSetId(1);
+		test.setQuestionText("A?");
+		test.setQuestionType("SINGLE_CHOICE");
+		test.setAnswersVar(answersVar);
+		test.setCorrectAnswers(correctAnswers);
+		
+		
+	}
+	
+	
+
+}
